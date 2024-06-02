28b8c5a8c6d8c5d0a1ac011454ad661e5204a716
goooguska <cahalego@mail.ru>
2024-06-02 13:35:54 +0300

    initial commit

---
+++ resources/js/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { RouterView } from "vue-router";
+</script>
+
+<template>
+    <h1>header</h1>
+
+    <RouterView> </RouterView>
+</template>
+
+<style scoped></style>
---
+++ resources/js/Pages/TestRoute.vue
@@ -0,0 +1,7 @@
+<script setup></script>
+
+<template>
+    <h1>I'm here to test!</h1>
+</template>
+
+<style scoped></style>
